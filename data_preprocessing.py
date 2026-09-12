812057c3413d444b1d1255eb1d79a3865c553797
Pratyush Pradhan <pratyush.pradhan1801@gmail.com>
2026-09-12 20:11:47 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,7 +2,7 @@
 # Load the social media CSV and inspect basic structure for the project writeup
 import pandas as pd
 
-file_path = 'social media - social_media_10000_outliers_duplicates.csv'
+file_path = '../data/social media - social_media_10000_outliers_duplicates.csv'
 
 social_df = pd.read_csv(file_path, encoding='ascii')
 
